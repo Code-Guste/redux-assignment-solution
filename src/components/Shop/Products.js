0d55e9de7321a3f5ac5ec7e73a90851177a6de3b
unknown <auguste.uzielaite@gmail.com>
2021-09-08 23:08:47 +0300

    Render dummy product list

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,23 @@
 import ProductItem from "./ProductItem";
 import classes from "./Products.module.css";
+import { useSelector } from "react-redux";
 
 const Products = (props) => {
+  const productList = useSelector((state) => state.cart.productList);
+
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title="Test"
-          price={6}
-          description="This is a first product - amazing!"
-        />
+        {productList.map((product) => (
+          <ProductItem
+            key={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+            id={product.id}
+          />
+        ))}
       </ul>
     </section>
   );
--- src/store/cart.js
+++ src/store/cart.js
@@ -2,6 +2,29 @@ import { createSlice } from "@reduxjs/toolkit";
 
 export const initialState = {
   show: false,
+  productList: [
+    {
+      id: 1,
+      title: "T-shirt",
+      price: 12,
+      description: "Oversized t-shirt",
+      quantity: 1,
+    },
+    {
+      id: 2,
+      title: "Hoodie",
+      price: 20,
+      description: "Stylish unisex hoodie",
+      quantity: 1,
+    },
+    {
+      id: 3,
+      title: "Jeans",
+      price: 25,
+      description: "Cropped women jeans",
+      quantity: 1,
+    },
+  ],
 };
 const cartSlice = createSlice({
   name: "cart",
